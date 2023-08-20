03d7979d87112c619d1e95acacf032721eb3ae6f
xHenriquemm <104220296+xHenriquemm@users.noreply.github.com>
2023-08-20 17:06:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const panels = document.querySelectorAll(".panel");
 
-    panels.forEach((panel) => {
-        panel.addEventListener("click", () => {
-            removeActiveClasses();
-            panel.classList.add("active");
-        });
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
     });
-        const removeActiveClasses = () => {
-            panels.forEach((panel) => {
-                panel.classList.remove("ative");
-            });
-            };
+});
+
+const removeActiveClasses = () => {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    });
+};
